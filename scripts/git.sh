8507c41c3cb4eda33ab9750dbb7d5b9c925ed534
Edmunt Pienkowsky <roed@poczta.onet.eu>
2022-09-04 10:52:27 +0200

    git: use -C switch

--- scripts/git.sh
+++ scripts/git.sh
@@ -2,14 +2,14 @@
 # git.sh
 #
 
-if [ -n "$(git status --porcelain)" ]; then
-	echo 'There are uncommited changes'
+if [ -n "$(git -C ${TOP_DIR} status --porcelain)" ]; then
+	echo 'There are uncommited changes' 1>&2
 	exit 1
 fi
 
-readonly BUILD_STAMP=$(git log -n 1 '--pretty=format:%ct')
+readonly BUILD_STAMP=$(git -C ${TOP_DIR} log -n 1 '--pretty=format:%ct')
 if [ -z "${BUILD_STAMP}" ]; then
-	echo 'Unable to get timestamp of last commit'
+	echo 'Unable to get timestamp of last commit' 1>&2
 	exit 1
 fi
 
